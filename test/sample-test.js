9653e8f1126e84025df66c2339437c8e62dc6194
Danila Pisarev <jobdan@bk.ru>
2022-02-06 10:49:55 +0300

    Rewrited the tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,50 +1,52 @@
-const { expect, should, assert } = require("chai");
+const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const Contribution = require("../artifacts/contracts/Conrtibution.sol/Contribution.json");
 
-async function getContribution(signer) {
-  const contributionContractFactory = await ethers.getContractFactory(
-    "Contribution",
-    signer
-  );
-  const contribution = await contributionContractFactory.deploy();
-  await contribution.deployed();
-  return contribution;
+async function printBalanceOf(address, msg = "") {
+  const rawBalance = await ethers.provider.getBalance(address);
+  console.log(await ethers.utils.formatEther(rawBalance));
 }
 
-describe("Contribution", async function () {
-  it("There is no repeating addresses of benefactors in array", async () => {
-    const [acc1, acc2, acc3] = await ethers.getSigners();
-    const contribution = await getContribution(acc1);
-    const contributionContract = await new ethers.Contract(
-      contribution.address,
-      Contribution.abi,
-      acc1
-    );
+describe("Contribution", () => {
+  let ContributionFactory, contribution, owner, acc2, acc3;
 
-    await contributionContract
-      .connect(acc2)
-      .makeDonat({ value: ethers.utils.parseEther("1") });
-    await contributionContract
-      .connect(acc2)
-      .makeDonat({ value: ethers.utils.parseEther("1") });
-    expect(await contributionContract.getBenefactors()).to.eql([acc2.address]);
+  beforeEach(async () => {
+    [owner, acc2, acc3] = await ethers.getSigners();
+    ContributionFactory = await ethers.getContractFactory("Contribution");
+    contribution = await ContributionFactory.deploy();
+    await contribution.deployed();
   });
 
-  it("Should fail if sender is not an owner", async () => {
-    const [acc1, acc2, acc3] = await ethers.getSigners();
-    const contribution = await getContribution(acc1);
-    const contributionContract = await new ethers.Contract(
-      contribution.address,
-      Contribution.abi,
-      acc1
-    );
-    await contributionContract
-      .connect(acc2)
-      .makeDonat({ value: ethers.utils.parseEther("1") });
-    
-    await expect(
-      contributionContract.connect(acc3).sendTo(acc3.address, 1)
-    ).to.be.revertedWith("You are not owner");
+  const callMakeDonatFrom = async (benefactor, amount) => {
+    await contribution
+      .connect(benefactor)
+      .makeDonat({ value: ethers.utils.parseEther(amount.toString()) });
+  };
+
+  describe("Deployment", () => {
+    it("Should set the right owner", async () => {
+      expect(await contribution.owner()).to.equal(owner.address);
+    });
+  });
+
+  describe("Checking of the receive and send coins", () => {
+    it("Should unique benefactors", async () => {
+      await callMakeDonatFrom(acc2, 1);
+      await callMakeDonatFrom(acc2, 1);
+      expect(await contribution.getBenefactors()).to.eql([acc2.address]);
+    });
+
+    it("Should fail if not owner send coins", async () => {
+      await callMakeDonatFrom(acc2, 1);
+      await expect(
+        contribution.connect(acc2).sendTo(acc2.address, 1)
+      ).to.be.revertedWith("You are not owner");
+    });
+
+    it("Should fail if contribution doen't have enough coins", async () => {
+      await callMakeDonatFrom(acc2, 1);
+      await expect(
+        contribution.sendTo(acc2.address, ethers.utils.parseEther("1"))
+      ).to.be.revertedWith("There is no such amount of coins");
+    });
   });
 });
