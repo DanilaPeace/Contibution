802e9a94939d399b3c7a47441597a72912e0e519
Danila Pisarev <jobdan@bk.ru>
2022-02-06 09:56:23 +0300

    Added the test to check only owner can transfer coins

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,4 +1,4 @@
-const { expect } = require("chai");
+const { expect, should, assert } = require("chai");
 const { ethers } = require("hardhat");
 const Contribution = require("../artifacts/contracts/Conrtibution.sol/Contribution.json");
 
@@ -13,7 +13,6 @@ async function getContribution(signer) {
 }
 
 describe("Contribution", async function () {
- 
   it("There is no repeating addresses of benefactors in array", async () => {
     const [acc1, acc2, acc3] = await ethers.getSigners();
     const contribution = await getContribution(acc1);
@@ -32,7 +31,20 @@ describe("Contribution", async function () {
     expect(await contributionContract.getBenefactors()).to.eql([acc2.address]);
   });
 
-  it("Only owner can send the coins", async () => {
-
+  it("Should fail if sender is not an owner", async () => {
+    const [acc1, acc2, acc3] = await ethers.getSigners();
+    const contribution = await getContribution(acc1);
+    const contributionContract = await new ethers.Contract(
+      contribution.address,
+      Contribution.abi,
+      acc1
+    );
+    await contributionContract
+      .connect(acc2)
+      .makeDonat({ value: ethers.utils.parseEther("1") });
+    
+    await expect(
+      contributionContract.connect(acc3).sendTo(acc3.address, 1)
+    ).to.be.revertedWith("You are not owner");
   });
 });
